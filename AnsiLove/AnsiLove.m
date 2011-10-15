877ec18858b411bb6690f438bb4a235f768587d2
ByteProject <stefan.vogt@byteproject.net>
2011-10-15 03:02:11 +0200

    Revert 77ab84763bb767166ca7f1de196895a70a4f5d63^..HEAD

---
+++ AnsiLove/AnsiLove.m
@@ -0,0 +1,23 @@
+//
+//  AnsiLove.m
+//  AnsiLove
+//
+//  Created by Stefan Vogt on 15.10.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AnsiLove.h"
+
+@implementation AnsiLove
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
