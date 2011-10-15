9d73823efe17e2528c06abeea27ed9e3ca4b9204
ByteProject <stefan.vogt@byteproject.net>
2011-10-15 02:49:56 +0200

    Initial Commit

---
+++ AnsiLove/AnsiLove.h
@@ -0,0 +1,13 @@
+//
+//  AnsiLove.h
+//  AnsiLove
+//
+//  Created by Stefan Vogt on 15.10.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnsiLove : NSObject
+
+@end
