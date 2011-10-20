48b6f810104071f9c33bc907b5345d5afba7e398
ByteProject <stefan.vogt@byteproject.net>
2011-10-20 16:37:44 +0200

    Revert 0b3ca9e633661f1417a01d45f332ea43df244c58^..HEAD

--- AnsiLoveGUI/ALAppDelegate.h
+++ AnsiLoveGUI/ALAppDelegate.h
@@ -9,9 +9,35 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <Ansilove/AnsiLove.h>
+// See that? Import the AnsiLove.framework as done above.
 
 @interface ALAppDelegate : NSObject <NSApplicationDelegate>
 
+// outlets
 @property (strong) IBOutlet NSWindow *window;
+@property (strong) IBOutlet NSTextField *inputField;
+@property (strong) IBOutlet NSTextField *outputField;
+@property (strong) IBOutlet NSTextField *columnsField;
+@property (strong) IBOutlet NSTextField *fontsField;
+@property (strong) IBOutlet NSTextField *bitsField;
+@property (strong) IBOutlet NSButton *iceColorsCheck;
+@property (strong) IBOutlet NSButton *columnsCheck;
+
+// strings
+@property (strong) NSString *inputFile;
+@property (strong) NSString *outputFile;
+@property (strong) NSString *columns;
+@property (strong) NSString *font;
+@property (strong) NSString *bits;
+@property (strong) NSString *iceColors;
+
+// integer and float values
+@property (assign) BOOL shouldUseIceColors;
+@property (assign) BOOL enableColumnsField;
+
+// actions
+- (IBAction)createPNGfromANSi:(id)sender;
+- (IBAction)clearColumnsField:(id)sender;
 
 @end
