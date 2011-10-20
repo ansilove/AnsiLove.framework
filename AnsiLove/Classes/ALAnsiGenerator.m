a73284714aa6e7874b56b85e4e3a9243115ed5c7
ByteProject <stefan.vogt@byteproject.net>
2011-10-20 16:40:15 +0200

    Revert "Revert 48b6f810104071f9c33bc907b5345d5afba7e398^..HEAD"
    This reverts commit bb9bdcb790e91775d3a175b277034b8099448e3f.

--- AnsiLove/Classes/ALAnsiGenerator.m
+++ AnsiLove/Classes/ALAnsiGenerator.m
@@ -22,21 +22,41 @@
                            font:(NSString *)font 
                            bits:(NSString *)bits
                       iceColors:(NSString *)iceColors
-{
-    NSMutableArray *arguments = [NSMutableArray arrayWithObjects:inputFile, outputFile, nil];
-    if (columns != nil || columns != @"") {
+{ 
+    if (inputFile == nil || inputFile == @"") {
+        // No inputfile? This means we can't do anything. Get the hell outta here.
+        return;
+    }
+    
+    if (outputFile == nil || outputFile == @"") {
+        // In case the user provided no output file / path, just use the file name and
+        // path from the inputFile value but add .png as suffix.
+        outputFile = [[NSString alloc] initWithFormat:@"%@.png", inputFile];
+    }
+    
+    // Initialize the arguments array.
+    NSMutableArray *arguments = [[NSMutableArray alloc] init];
+    
+    // Add the two most necessary arguments to the array.
+    [arguments addObject:inputFile];
+    [arguments addObject:outputFile];
+    
+    // The following if statements check if a string is nil or empty. That way we can
+    // be sure only strings with proper contents will be added to the arguments array.
+    if (columns && ![columns isEqualToString:@""]) {
         [arguments addObject:columns];
     }
-    if (font != nil || font != @"") {
+    if (font && ![font isEqualToString:@""]) {
         [arguments addObject:font];
     }
-    if (bits != nil || bits != @"") {
+    if (bits && ![bits isEqualToString:@""]) {
         [arguments addObject:bits];
     }
-    if (iceColors != nil || iceColors != @"") {
+    if (iceColors && [iceColors isEqualToString:@"1"]) {
         [arguments addObject:iceColors];
     }
     
+    // Finally start the task with the commandline arguments we gathered.
     NSPipe *pipe;
     pipe = [NSPipe pipe];
     
--- AnsiLoveGUI/ALAppDelegate.m
+++ AnsiLoveGUI/ALAppDelegate.m
@@ -12,11 +12,57 @@
 
 @implementation ALAppDelegate
 
-@synthesize window = _window;
+@synthesize window = _window, inputField, outputField, columnsField, fontsField, bitsField,
+            iceColorsCheck, columnsCheck, inputFile, outputFile, columns, font, bits, iceColors,
+            shouldUseIceColors, enableColumnsField;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    // When the ANSi source does not make specific use of iCE colors,
+    // you should NOT set this flag. Also: NO is default in AnsiLove. Comprende?  
+    self.shouldUseIceColors = NO;
+    
+    // The property is bound to the enabled binding of the columnsField. If you check 
+    // columns in the GUI, the field gets enabled and you can enter a column value.
+    // Note that this has no educational purpose for the AnsiLove.framework, it's
+    // just a nice feature I recommend as the columns flag is only needed for .bin files.
+    self.enableColumnsField = NO;
+}
+
+// This method gets invoked by the 'Generate' button. It collects all the content the user
+// entered into the text fields and calls the framework. The synthesized string values above
+// (inputFile, outputFile, columns, font and bits) are all bound to the corresponding 
+// value binding of the text field instances you can see in the UI.
+- (IBAction)createPNGfromANSi:(id)sender 
+{
+    if (shouldUseIceColors == NO) {
+        // AnsiLove needs all flags as strings, that's why we can't pass the bool value 
+        // (or any other integer) directly. You can do something like I did below.
+        self.iceColors = @"0";
+    }
+    else {
+        self.iceColors = @"1";
+    }
+    
+    // Let's finally pass all we got to the AnsiLove.framework, it will do the rest.
+    // If you don't want to set a specific flag, like the font flag for example, just
+    // pass nil or an empty NSString like @"". Both will work and AnsiLove will use
+    // it's built-in default values for generating the output PNG.
+    [ALAnsiGenerator createPNGFromAnsiSource:self.inputFile 
+                                  outputFile:self.outputFile 
+                                     columns:self.columns 
+                                        font:self.font 
+                                        bits:self.bits 
+                                   iceColors:self.iceColors];
+}
+
+- (IBAction)clearColumnsField:(id)sender 
+{
+    // The method clears the columns field as soon as it get's disabled. That way
+    // we make sure that no content is passed when the columns flag is not needed.
+    if ([columnsField isEnabled] == NO) {
+        self.columns = @"";
+    }
 }
 
 @end
