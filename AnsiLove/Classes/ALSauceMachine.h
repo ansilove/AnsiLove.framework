c4d58b1fa3af6ce8afd5ac53b2d1f2083af11e14
ByteProject <stefan.vogt@byteproject.net>
2011-11-24 11:17:31 +0100

    initial commit of the classes intended to manipulate sauce records

---
+++ AnsiLove/Classes/ALSauceMachine.h
@@ -0,0 +1,16 @@
+//
+//  ALSauceMachine.h
+//  AnsiLove.framework
+//
+//  Copyright (c) 2011, Stefan Vogt. All rights reserved.
+//  http://byteproject.net
+//
+//  Use of this source code is governed by a MIT-style license.
+//  See the file LICENSE for details.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ALSauceMachine : NSObject
+
+@end
---
+++ AnsiLove/Classes/ALSauceMachine.m
@@ -0,0 +1,16 @@
+//
+//  ALSauceMachine.m
+//  AnsiLove.framework
+//
+//  Copyright (c) 2011, Stefan Vogt. All rights reserved.
+//  http://byteproject.net
+//
+//  Use of this source code is governed by a MIT-style license.
+//  See the file LICENSE for details.
+//
+
+#import "ALSauceMachine.h"
+
+@implementation ALSauceMachine
+
+@end
