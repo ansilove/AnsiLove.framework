bb94309f5b78fc6b79eb2bc2a5c8dcbda8226604
ByteProject <stefan.vogt@byteproject.net>
2013-08-11 01:02:57 +0200

    adding private methods to ALAnsiGenerator

---
+++ AnsiLove/Classes/ALAnsiGenerator+PrivateMethods.m
@@ -0,0 +1,17 @@
+//
+//  ALAnsiGenerator+PrivateMethods.m
+//  AnsiLove.framework
+//
+//  Copyright (c) 2011-2013, Stefan Vogt. All rights reserved.
+//  http://byteproject.net
+//
+//  Use of this source code is governed by a MIT-style license.
+//  See the file LICENSE for details.
+//
+
+
+#import "ALAnsiGenerator+PrivateMethods.h"
+
+@implementation ALAnsiGenerator (PrivateMethods)
+
+@end
