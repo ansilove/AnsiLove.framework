b52114fb55c1cbd6f4cc8148cce5268b62b6b2f2
ByteProject <stefan.vogt@byteproject.net>
2013-08-31 20:16:39 +0200

    wiping libsauce related code, using internal ALSauceMachine now

--- AnsiLove/Classes/ALAnsiLove.h
+++ AnsiLove/Classes/ALAnsiLove.h
@@ -16,6 +16,7 @@
 #import "ALSubStr.h"
 #import "ALExplode.h"
 #import "ALFileSize.h"
+#import "ALSauceMachine.h"
 
 #if !defined(MIN)
 #define MIN(A,B) ({ __typeof__(A) __a = (A); __typeof__(B) __b = (B); __a < __b ? __a : __b; })
@@ -56,34 +57,3 @@ struct ansiChar {
     bool italics;
     bool underline;
 };
-
-// sauce records
-#define RECORD_SIZE  128
-#define COMMENT_SIZE 64
-#define SAUCE_ID     "SAUCE"
-#define COMMENT_ID   "COMNT"
-
-typedef struct {
-    char             ID[6];
-    char             version[3];
-    char             title[36];
-    char             author[21];
-    char             group[21];
-    char             date[9];
-    int32_t          fileSize;
-    unsigned char    dataType;
-    unsigned char    fileType;
-    unsigned short   tinfo1;
-    unsigned short   tinfo2;
-    unsigned short   tinfo3;
-    unsigned short   tinfo4;
-    unsigned char    comments;
-    unsigned char    flags;
-    char             filler[23];
-    char             **comment_lines;
-} sauce;
-
-sauce *sauceReadFileName(char *fileName);
-sauce *sauceReadFile(FILE *file);
-void  readRecord(FILE *file, sauce *record);
-void  readComments(FILE *file, char **comment_lines, int32_t comments);
